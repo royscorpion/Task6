46acf7280fd4632b502355814b1e5d7c0e4bf04d
Виктор Парыгин <paryginva@live.com>
2021-11-23 23:56:39 +0500

    Добавьте файлы проекта.

---
+++ Task6.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Поиск самого длинного слова в введенной строке
+            #region Исходные данные
+            string str = Console.ReadLine(), longWord = "";
+            string[] stringArray = str.Split();
+            int strLen, strMaxLen = 0;
+            #endregion
+
+            #region Разбивка строки на отдельные слова, определение самого длинного слова
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (strMaxLen < (strLen = stringArray[i].Length))
+                {
+                    strMaxLen = strLen;
+                    longWord = stringArray[i];
+                }
+            }
+            #endregion
+
+            //Вывод результатов
+            Console.WriteLine("Самое длинное слово в введеной строке - {0}\nДлина слова - {1} символов", longWord,strMaxLen);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task6.2_PalindromeDetermine/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Определение, является ли введенная строка палиндромом без учета регистра и пробелов
+            #region Ввод исходных данных, переформатирование введенной строки
+            string str = Console.ReadLine().ToUpper().Replace(" ", "");
+            bool palindrome = true;
+            #endregion
+
+            #region Исследование строки
+            while (str.Length > 1)
+            {
+                if (!str.EndsWith(str.Substring(0, 1)))
+                {
+                    palindrome = false;
+                    break;
+                }
+                else
+                {
+                    str = str.Substring(1, str.Length-2);
+                }
+            }
+            #endregion
+
+            Console.WriteLine(palindrome?"Введенная строка является палиндромом":"Введенная строка не является палиндромом");
+            Console.ReadKey();
+        }
+    }
+}
